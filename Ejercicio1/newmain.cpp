e79eb9c1fcc659a612ed85a3ffdbcfff4bbf719d
INTERNET3 <INTERNET3@INTERNET3-PC>
2014-05-25 10:46:00 -0500

    Creación del Proyecto

---
+++ Ejercicio1/newmain.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   newmain.cpp
+ * Author: INTERNET3
+ *
+ * Created on 25 de mayo de 2014, 10:44
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
